e3aef2fd57e0df3a06d0b0be8d4b47cd10d07c25
Jorge <fulano@email.com>
2022-07-04 07:24:12 -0300

    requisitos cumpridos com ajuste

--- src/middlewares/signUpValidation.js
+++ src/middlewares/signUpValidation.js
@@ -1,10 +1,18 @@
 import signUpSchema from "../schemas/signUpSchema.js";
+import db from '../db.js';
 
 export default function signUpValidation(req, res, next) {
   const validation = signUpSchema.validate(req.body);
   if (validation.error) {
-    return res.sendStatus(validation.error.details);
+    return res.sendStatus(422);
   }
-
+  const user = db.collection('users').findOne({name:req.body.name});
+  if(user){
+    return res.sendStatus(409);
+  }
+  const user2 = db.collection('users').findOne({email:req.body.email});
+  if(user2){
+    return res.sendStatus(409);
+  }
   next();
 }
