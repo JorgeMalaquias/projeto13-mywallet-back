1eeb543cea35ae849854ae5ce52cb0f3b69f8b6d
Jorge <fulano@email.com>
2022-07-04 07:18:51 -0300

    requisitos cumpridos

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -23,7 +23,11 @@ export async function signIn(req, res) {
         const user = await db.collection('users').findOne({ email });
 
         if (user && bcrypt.compareSync(password, user.password)) {
-            await db.collection('sessions').deleteMany({userId:user._id})
+            const session = await db.collection('sessions').findOne({ userId: user._id });
+
+            if (session) {
+                await db.collection('sessions').deleteOne({ userId: user._id });
+            }
             const token = uuid();
 
             await db.collection('sessions').insertOne({ token, userId: user._id });
@@ -32,9 +36,22 @@ export async function signIn(req, res) {
         } else {
             res.sendStatus(401);
         }
-    }catch (error) {
+    } catch (error) {
         res.status(500).send(error);
     }
 
-    
+
+}
+
+export async function loggingOut(req, res) {
+    const { token } = req.params;
+
+    try {
+
+        await db.collection('sessions').deleteOne({ token:token})
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.status(500).send(error)
+    }
 }
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,8 +3,10 @@ import signUpValidation from "../middlewares/signUpValidation.js";
 import signInValidation from "../middlewares/signInValidation.js";
 import { signUp } from "../controllers/authControllers.js";
 import { signIn } from "../controllers/authControllers.js";
+import { loggingOut } from "../controllers/authControllers.js";
 
 const authRouter = Router();
 authRouter.post("/sign-up", signUpValidation, signUp);
 authRouter.post("/sign-in", signInValidation, signIn);
+authRouter.delete('/log-out/:token', loggingOut);
 export default authRouter;
