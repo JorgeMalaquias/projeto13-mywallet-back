65263da84f45aa129577f3ea92896f1fcca606c7
jorge <jorgemalaquia@gmail.com>
2023-02-08 19:37:04 -0300

    feat: adding error handler widdleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './routes/index.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { errorHandlerMiddleware } from './middlewares/errorHandler.js';
 dotenv.config();
 
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(router);
+app.use(errorHandlerMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
