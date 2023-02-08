08d30ff84dd9fad7e71abafbf3148237155c7583
jorge <jorgemalaquia@gmail.com>
2023-02-08 19:38:03 -0300

    refactor: change name router to routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import router from './routes/index.js';
+import routes from './routes/index.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { errorHandlerMiddleware } from './middlewares/errorHandler.js';
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(router);
+app.use(routes);
 app.use(errorHandlerMiddleware);
 
 app.listen(process.env.PORT, () => {
